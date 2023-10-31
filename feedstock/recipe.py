606e125577bae7f1bf2ae43aa7fd8f46ddb21fb9
ranchodeluxe <greg.corradini@gmail.com>
2023-10-31 07:46:24 -0700

    try with pattern

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,7 +1,7 @@
 import base64
 import json
 import os
-
+import pandas as pd
 from apache_beam import Create, PTransform, PCollection, Map
 from cmr import GranuleQuery
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
@@ -14,20 +14,20 @@ CREDENTIALS_API = "https://archive.podaac.earthdata.nasa.gov/s3credentials"
 PODAAC_BUCKET = "podaac-ops-cumulus-protected"
 MURSST_PREFIX = "MUR-JPL-L4-GLOB-v4.1"
 
+dates = [
+    d.to_pydatetime().strftime('%Y%m%d')
+    for d in pd.date_range("1996-10-01", "1999-02-01", freq="D")
+]
 
-def gen_data_links():
-    granules = GranuleQuery().short_name("MUR-JPL-L4-GLOB-v4.1").downloadable(True).get_all()
-    for granule in granules:
-        data_link = next(filter(lambda link: link["rel"] == HTTP_REL, granule["links"]))
-        yield data_link["href"]
+def make_filename(time):
+    base_url = "https://archive.podaac.earthdata.nasa.gov/podaac-ops-cumulus-protected/MUR-JPL-L4-GLOB-v4.1/"
+    # example file: "/20020601090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc"
+    return f"{base_url}{time}090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc"
 
+concat_dim = ConcatDim("time", dates, nitems_per_file=1)
+pattern = FilePattern(make_filename, concat_dim)
 
-pattern = pattern_from_file_sequence(
-    list(gen_data_links()),
-    "time",
-)
-
-open_kwargs = {'Authorization': f'Bearer {os.environ["EARTHDATA_TOKEN"]}'}
+open_kwargs = {"headers":{'Authorization': f'Bearer {os.environ["EARTHDATA_TOKEN"]}'}}
 
 mursst = (
     Create(pattern.items())
@@ -36,7 +36,6 @@ mursst = (
     | StoreToZarr(
         store_name="mursst.zarr",
         combine_dims=pattern.combine_dim_keys,
-        target_chunks={"time": 1, "lat": 1800, "lon": 3600},
     )
 )
 
