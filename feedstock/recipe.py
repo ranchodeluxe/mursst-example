789e4429d30d94f8c63587e42eac5e190cb595d0
ranchodeluxe <greg.corradini@gmail.com>
2023-11-01 05:04:06 -0700

    try DropVars and target_chunks

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import os
+import apache_beam as beam
 import pandas as pd
 from apache_beam import Create, PTransform, PCollection, Map
 from cmr import GranuleQuery
@@ -19,6 +20,8 @@ dates = [
     for d in pd.date_range("2002-06-01", "2002-06-05", freq="D")
 ]
 
+target_chunks = {"time": 40}
+
 def make_filename(time):
     base_url = "https://archive.podaac.earthdata.nasa.gov/podaac-ops-cumulus-protected/MUR-JPL-L4-GLOB-v4.1/"
     # example file: "/20020601090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc"
@@ -29,13 +32,30 @@ pattern = FilePattern(make_filename, concat_dim)
 
 open_kwargs = {"headers":{'Authorization': f'Bearer {os.environ["EARTHDATA_TOKEN"]}'}}
 
+
+class DropVars(beam.PTransform):
+    """
+    Custom Beam transform to drop unused vars
+    """
+    @staticmethod
+    def _drop_vars(item: Indexed[T]) -> Indexed[T]:
+        index, ds = item
+        ds = ds.drop_vars(["analysed_sst", "analysis_error", "mask"])
+        return index, ds
+
+    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
+        return pcoll | beam.Map(self._drop_vars)
+
+
 mursst = (
     Create(pattern.items())
     | OpenURLWithFSSpec(open_kwargs=open_kwargs)
     | OpenWithXarray(file_type=pattern.file_type)
+    | DropVars()
     | StoreToZarr(
         store_name="mursst.zarr",
         combine_dims=pattern.combine_dim_keys,
+        target_chunks=target_chunks,
     )
 )
 
